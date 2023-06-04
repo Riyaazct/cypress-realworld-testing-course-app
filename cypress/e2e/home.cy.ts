02824e5459a5eadefa273972faab0183b4c64b3e
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-06-05 01:00:18 +0200

    tests for multiple pages complete

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,17 +3,39 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct text", () => {
-    cy.getByData("hero-heading").contains(
-      "Testing Next.js Applications with Cypress"
-    )
-  })
+  context("hero section", () => {
+    it("the h1 contains the correct text", () => {
+      cy.getByData("hero-heading").contains(
+        "Testing Next.js Applications with Cypress"
+      )
+    })
 
-  it("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt")
-      .eq(2)
-      .contains(/free and Open source/i)
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt")
+        .eq(2)
+        .contains(/free and Open source/i)
+    })
+  })
+  /*
+  - How to test multiple pages
+  - https://learn.cypress.io/testing-your-first-application/how-to-test-multiple-pages
+  */
+  context("Courses section", () => {
+    it("Course: Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-your-first-application")
+    })
+
+    it("Course: Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/testing-foundations")
+    })
+
+    it("Course: Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("equal", "/cypress-fundamentals")
+    })
   })
 })
